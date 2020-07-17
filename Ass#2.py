9dac330d0a99c07d4680d431e39af3ad4e70a743
Mohammad_Talha <57532114+talhaansari77@users.noreply.github.com>
2020-07-17 08:44:23 +0500

    python fucntion
    created some basic functions

---
+++ Ass#2.py
@@ -0,0 +1,32 @@
+#This is Assignment# 2
+
+
+
+
+#this fuction will return the name of artist
+def artist(artist_name):
+    return artist_name
+
+#this fuction will return the name of song
+def song_name(name):
+    return name
+
+#this fuction will return the release date
+def year(release):
+    return release
+
+#this fuction will return the total views 
+def views(view):
+    return view
+
+
+
+#This is my main Fuction
+
+print("Song: "   + artist("aa rha ho main"))
+
+print("Artist: " + artist("Atif Aslam"))
+
+print("Views: "  + str(artist(325)) +" Million")
+
+print("Year: "   + str(artist(2015)))
