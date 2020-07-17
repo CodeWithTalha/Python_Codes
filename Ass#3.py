0b6690c9e54294ba3dc002634da98fb8e423330f
Mohammad_Talha <57532114+talhaansari77@users.noreply.github.com>
2020-07-17 10:52:35 +0500

    find_equal return function
    this file contain a return type function which returns true if numbers are equal

---
+++ Ass#3.py
@@ -0,0 +1,13 @@
+#This is Assignment# 3
+
+#This Fucntin Will return True or False based on the condition 
+def find_equal(a,b,c):
+    if a == b or a == c or b == c:
+        return True
+    else:
+        return False
+
+
+#This is my main Function
+
+print(find_equal(3,5,5))
